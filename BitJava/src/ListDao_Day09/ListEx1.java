0d0756fb1c5938a97286d9b873795b1b2fb20f5d
jindream6128 <jindream6128@naver.com>
2023-04-09 19:13:25 +0900

    NumberFormatException

--- BitJava/src/ListDao_Day09/ListEx1.java
+++ BitJava/src/ListDao_Day09/ListEx1.java
@@ -17,34 +17,49 @@ import java.util.Scanner;
 //int no, String title, String writer, STring contents, String password, String regdate
 
 public class ListEx1 {
-	private Scanner sc = new Scanner(System.in);
 	private ArrayList list;
 	static final String PATH = "D:\\testForder\\test.txt";
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+	
+	//숫자아닌거 예외처리
+	public int ExceptionBuffredReader() throws Exception {
+		int n = 0,  flag;
+		do {
+			flag =1;
+			try {
+				n = Integer.parseInt(br.readLine());
+			}catch(Exception e) {
+				flag = 0;
+				System.out.println("숫자를 입력해 주세요");
+			}
+		}while(flag == 0);
+		
+		return n;
+	}
+	
 	// input Data -> input하고 BoardBean bean return
-	public BoardBean inputData() {
+	public BoardBean inputData() throws Exception {
 		BoardBean bean = new BoardBean();
 		int no = 0;
 
 		do {
 			System.out.println("id를 입력해 주세요");
-			no = sc.nextInt();
+			no = ExceptionBuffredReader();
 		} while (!ListDao.getDao().noCheck(no));
 
 		bean.setNo(no);
 
 		System.out.println("title을 입력하세요");
-		bean.setTitle(sc.next());
+		bean.setTitle(br.readLine());
 
 		System.out.println("writer를 입력하세요");
-		bean.setWriter(sc.next());
+		bean.setWriter(br.readLine());
 
 		System.out.println("contents를 입력하세요");
-		bean.setContents(sc.next());
+		bean.setContents(br.readLine());
 
 		System.out.println("password를 입력하세요");
-		bean.setPassword(sc.next());
+		bean.setPassword(br.readLine());
 
 		System.out.println("regdate 등록일자 입니다");
 		Calendar c = Calendar.getInstance();
@@ -62,12 +77,12 @@ public class ListEx1 {
 		System.out.println(bean);
 	}
 
-	public int search() {
-		System.out.println("ID Number: ");
-		int idNum = sc.nextInt();
+	public int search() throws Exception {
+		System.out.println("찾고싶은 ID를 입력해 주세요");
+		int idNum = ExceptionBuffredReader();
 		int index = ListDao.getDao().getIdNumIndex(idNum);
 		if (index == -1) {
-			System.out.println("없음 ID입니다");
+			System.out.println("없는 ID입니다");
 		} else {
 			System.out.println(ListDao.getDao().getIndexBean(index));
 		}
@@ -83,10 +98,10 @@ public class ListEx1 {
 	}
 
 	// modify -> 찾는 id값을 입력받고, 그index받아서 삭제하고, 그자리에 그냥 전부 다시 입력받기 그리고 출력하기
-	public void modify() {
+	public void modify() throws Exception {
 		System.out.println("수정할 ID 값을 입력해 주세요");
 		// 수정할 id값
-		int idNum = sc.nextInt();
+		int idNum = ExceptionBuffredReader();
 		// 수정할 인덱스
 		int idIndex = ListDao.getDao().getIdNumIndex(idNum);
 		if (idIndex == -1) {
@@ -97,9 +112,9 @@ public class ListEx1 {
 		ListDao.getDao().listModify(idIndex, inputData());
 	}
 
-	public void delete() {
+	public void delete() throws Exception {
 		System.out.println("삭제할 ID 값을 입력해 주세요, 만약 전체 삭제를 원하신다면 -1을 입력해 주세요");
-		int idNum = sc.nextInt();
+		int idNum = ExceptionBuffredReader();
 
 		if (idNum == -1) {
 			ListDao.getDao().allDelete();
@@ -114,7 +129,7 @@ public class ListEx1 {
 	}
 
 	public void Save() throws IOException {
-		System.out.println("저장할 경로를 입력해 주세요 D:\\testForder\\test.txt");
+		System.out.println("저장할 경로를 입력해 주세요");
 		String path = br.readLine();
 		try (ObjectOutputStream oos = new ObjectOutputStream(new PrintStream(new File(path)))) {
 			oos.writeObject(ListDao.getDao().getList());
@@ -126,7 +141,7 @@ public class ListEx1 {
 	}
 
 	public void Open() throws IOException {
-		System.out.println("불러올 파일의 경로를 입력해 주세요. D:\\testForder\\test.txt");
+		System.out.println("불러올 파일의 경로를 입력해 주세요.");
 		String path = br.readLine();
 		try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File(path)))) {
 			ListDao.setList((ArrayList)ois.readObject());
@@ -146,7 +161,6 @@ public class ListEx1 {
 			list();
 		} catch (Exception e) {
 			System.out.println("저장된 데이터가 없습니다.");
-			e.printStackTrace();
 		}
 	}
 
@@ -172,7 +186,7 @@ public class ListEx1 {
 			// 9 -> 시스템 종료 ㅇ
 			System.out.println("0.AutoCall 1.Add 2.List(All print) 3.Info 4.Modify 5.Delete 6.Save 7.Open 8.AutoSave 9.SystemExit");
 			
-			switch (sc.nextInt()) {
+			switch (ExceptionBuffredReader()) {
 			case 0:
 				AutoOpen();
 				break;
